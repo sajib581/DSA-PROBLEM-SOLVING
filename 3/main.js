5d6df5d124735f4f5d4a90c7111e59f6fbca5813
sajib581 <sajibdas581@gmail.com>
2022-06-20 00:46:59 +0600

    class 3

--- 3/main.js
+++ 3/main.js
@@ -1,32 +1,34 @@
-// Array and Object performance - BigO
-// how to approach a problem
-const arr = ['a', 'b', 'c'];
-
-// console.log(arr.length);
-// console.log(arr[2]);
-
-// arr.push('d')
-// console.log(arr);
-
-// arr.pop()
-// console.log(arr);
-
-//add in the start
-
-// arr.unshift('z')
-// console.log(arr);
-
-// arr.shift()
-// console.log(arr);
-
-//OBJECT
-const obj = {
-    name : 'Sajib',
-    email:"sajibdas581@gmail.com"
-}
-
-for (const key in obj) {
-    console.log(obj[key]);   
-}
 
 //problem Solving Approach    
+// input- arr1-['a', 'b', 'c'] arr2-[1, 2, 3, 'z'] //false
+// input- arr1-['a', 'b', 'c'] arr2-[1, 2, 3, 'c'] //true
+//In general technique
+function isElementExists(arr1, arr2) {
+    //flag variable
+    let exists = false
+    for(let elm of arr1){
+       if(arr2.includes(elm)){
+        exists = true
+        break
+       }
+    }
+ 
+    return exists
+  }
+  console.log(isElementExists(['a', 'b', 'c'], [1, 2, 3, 'b']))
+
+  // time complexity O(n + m)
+//space complexity O(n)
+function isElementExists(arr1, arr2) {
+    const frequencyCounter = {}
+    for (let elm of arr1) {
+      frequencyCounter[elm] = true
+    }
+   
+    for (let elm of arr2) {
+      if (elm in frequencyCounter) {
+        return true
+      }
+    }
+  }
+  console.log(isElementExists(['a', 'b', 'c'], [1, 2, 3, 'b']))
