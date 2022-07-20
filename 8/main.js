60cce43636f1f343794cff533347aa4e455a75fc
sajib581 <sajibdas581@gmail.com>
2022-07-20 11:02:14 +0600

    pyramid

--- 8/main.js
+++ 8/main.js
@@ -1,3 +1,4 @@
+//Steps Pattern
 //Step(2)
 //    '#'
 //    '##'
@@ -27,4 +28,39 @@ function steps(n) {
     }
 }
 
-steps(5)
+// steps(5)
+
+function pyramid(n){
+    let midpoint = Math.floor((2*n - 1)/2)
+    for(let row = 0; row<n; row++){
+        let level = ''
+        for(let column = 0 ; column < 2*n-1; column++){
+                                                                    console.log(midpoint - row, column ,"B", midpoint+row);
+            if(midpoint - row <= column && midpoint+row >=column){
+                level+='#'
+            }else{
+                level += ' '
+            }
+        }
+        console.log(level);
+    }
+}
+
+// pyramid(3)
+
+function pyramidAlternative(n){
+    for (let row = 1; row <= n ; row++) {
+        let level = '' ;
+        //space
+        for(let column = 1; column<= n-row ; column++){
+            level+=' '
+        }    
+        //Stars
+        for(let star = 1; star<= 2*row-1 ; star++){
+            level+='#'
+        } 
+        console.log(level);
+    }
+}
+
+pyramidAlternative(5)
