1fa16f18daeee590887ee84d926cdead44a3c071
sajib581 <sajibdas581@gmail.com>
2022-07-21 12:19:17 +0600

    spine matrix leetcode

--- 8/main.js
+++ 8/main.js
@@ -63,4 +63,52 @@ function pyramidAlternative(n){
     }
 }
 
-pyramidAlternative(5)
+// pyramidAlternative(5)
+
+function matrix(n){
+    const results = []
+    for(let i=0; i<n ; i++){
+        results.push([])
+    }
+    let startColumn = 0;
+    let endColumn = n-1 ; 
+    let startRow = 0;
+    let endRow = n-1;
+    let counter = 1 ;
+    while (startColumn<= endColumn && startRow<=endRow ) {
+        //top row
+        for(let i=startColumn; i<=endColumn ; i++){
+            results[startRow][i] = counter
+            counter++
+        }
+        
+        startRow++
+        
+        //Right Column
+        for(let i= startRow; i<= endRow; i++){
+            results[i][endColumn] = counter
+            counter++
+        }
+        
+        endColumn-- ;
+
+        //Bottom Row
+        for(let i = endColumn; i>= startColumn; i--){
+            results[endRow][i] = counter
+            counter++
+        }
+        
+        endRow-- ;
+
+        //Start Column
+        for (let i = endRow; i>= startRow; i--) {
+            results[i][startColumn] = counter
+            counter++            
+        }
+        
+        startColumn++
+    }
+    console.log(results);
+}
+
+matrix(3)
---
+++ 8/test.js
@@ -0,0 +1,3 @@
+function matrix(params) {
+    
+}
