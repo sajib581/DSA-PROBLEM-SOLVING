c13f5a4b41cc63dd83c4cfd95725fd478201253d
sajib581 <sajibdas581@gmail.com>
2022-07-27 22:50:54 +0600

    merge sort

---
+++ 10/merge.js
@@ -0,0 +1,37 @@
+function merge(arr1, arr2){
+    let i =0 ;
+    let j=0 ;
+    const result = []
+    while(i<arr1.length && j<arr2.length ){
+        if(arr1[i]<arr2[j]){
+            result.push(arr1[i])
+            i++
+        }else{
+            result.push(arr2[j])
+            j++
+        }
+    }
+    while(i < arr1.length){
+        result.push(arr1[i])
+        i++
+    }
+    while(j < arr2.length){
+        result.push(arr2[j])
+        j++
+    }
+    return result;
+}
+
+//Time Complexity O(nlogn)
+//Space Complexity O(n)
+function mergeSort(arr){
+    let mid = Math.floor(arr.length/2)
+    if(arr.length <= 1) return arr
+    
+    let left = mergeSort(arr.slice(0, mid))
+    let right = mergeSort(arr.slice(mid))
+
+    return merge(left, right)
+}
+
+console.log(mergeSort([3,7,9,8,10]));
